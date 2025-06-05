fa441e653790b541c1463c6500c0e50489762ac8
sinehakarira <sinehavaswani@gmail.com>
2025-06-05 13:11:41 +0500

    Add files via upload

---
+++ pk.js
@@ -0,0 +1,6 @@
+publicKey = "pk_test_51RSN6QH9U0yAcZTctqlMKe2UTCMwchXSJwtK91HUwN9ylqexRmlIbN8u17xYxQOg4F7Pij1vUqn5umwd6DnxTBsb00KTOUex0D"
+// Card Number: 4242 4242 4242 4242
+
+// Expiry: Any future date (e.g., 12/34)
+
+// CVC: Any 3-digit number (e.g., 123)
